92c4e387c85781926f735e56f2b95d614ba8eb0b
xiangying <ying01.xiang@vipshop.com>
2015-08-25 21:30:30 +0800

    origin

---
+++ Hybird/Hybird.h
@@ -0,0 +1,19 @@
+//
+//  Hybird.h
+//  Hybird
+//
+//  Created by xiang ying on 15/8/25.
+//  Copyright (c) 2015年 xiang ying. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Hybird.
+FOUNDATION_EXPORT double HybirdVersionNumber;
+
+//! Project version string for Hybird.
+FOUNDATION_EXPORT const unsigned char HybirdVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hybird/PublicHeader.h>
+
+
